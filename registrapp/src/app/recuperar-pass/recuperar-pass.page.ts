959ed3f347281224de30eac03bfdaedd223e5a8b
RazielAuditore <107446288+RazielAuditore@users.noreply.github.com>
2023-11-08 23:32:06 -0300

    2 comiit
    commit numero 2

--- registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
+++ registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
-  selector: 'app-recuperar-pass',
+  selector: 'app-registro',
   templateUrl: './recuperar-pass.page.html',
   styleUrls: ['./recuperar-pass.page.scss'],
 })
 export class RecuperarPassPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  nombre: string="";
+  edad: number=0;
+  rut: string="";
+  carrera: string="";
+  email: string="";
+  contrasena: string="";
 
+  async guardarDatos() {
+    await Storage.set({
+      key: 'nombre',
+      value: this.nombre
+    });
+
+    await Storage.set({
+      key: 'edad',
+      value: this.edad.toString()
+    });
+
+    await Storage.set({
+      key: 'rut',
+      value: this.rut
+    });
+
+    await Storage.set({
+      key: 'carrera',
+      value: this.carrera
+    });
+
+    await Storage.set({
+      key: 'email',
+      value: this.email
+    });
+
+    await Storage.set({
+      key: 'contrasena',
+      value: this.contrasena
+    });
+  }
 }
