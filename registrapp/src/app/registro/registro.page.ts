0c09b0ccd98d6dc82ead9e14df90e83d679237c0
RazielAuditore <107446288+RazielAuditore@users.noreply.github.com>
2023-11-08 23:47:56 -0300

    recuperar clave
    3er commit

--- registrapp/src/app/registro/registro.page.ts
+++ registrapp/src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  nombre: string="";
+  edad: number=0;
+  rut: string="";
+  carrera: string="";
+  email: string="";
+  contrasena: string="";
 
+  async guardarDatos() {
+    await Storage.set({
+      key: 'nombre',
+      value: this.nombre
+    });
+
+    await Storage.set({
+      key: 'edad',
+      value: this.edad.toString()
+    });
+
+    await Storage.set({
+      key: 'rut',
+      value: this.rut
+    });
+
+    await Storage.set({
+      key: 'carrera',
+      value: this.carrera
+    });
+
+    await Storage.set({
+      key: 'email',
+      value: this.email
+    });
+
+    await Storage.set({
+      key: 'contrasena',
+      value: this.contrasena
+    });
+  }
 }
