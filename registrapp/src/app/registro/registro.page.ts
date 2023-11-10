bfbca415192feeea1e9c6b6f49033704b57266b5
BIGMARK96 <megamarchioni@gmail.com>
2023-11-10 00:36:32 -0300

    capacitor guarda mas de 1 usuario

--- registrapp/src/app/registro/registro.page.ts
+++ registrapp/src/app/registro/registro.page.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@capacitor/storage';
 
+export class Usuario {
+  nombre: string="";
+  edad: number=0;
+  rut: string="";
+  carrera: string="";
+  email: string="";
+  contrasena: string="";
+}
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -10,6 +19,7 @@ export class RegistroPage implements OnInit {
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+  usuarios: Usuario[] = [];
   nombre: string="";
   edad: number=0;
   rut: string="";
@@ -18,34 +28,24 @@ export class RegistroPage implements OnInit {
   contrasena: string="";
 
   async guardarDatos() {
-    await Storage.set({
-      key: 'nombre',
-      value: this.nombre
-    });
+    let usuario = new Usuario();
+    usuario.nombre = this.nombre;
+    usuario.edad = this.edad;
+    usuario.rut = this.rut;
+    usuario.carrera = this.carrera;
+    usuario.email = this.email;
+    usuario.contrasena = this.contrasena;
+
+    this.usuarios.push(usuario);
 
     await Storage.set({
-      key: 'edad',
-      value: this.edad.toString()
+      key: 'usuarios',
+      value: JSON.stringify(this.usuarios)
     });
+  }
 
-    await Storage.set({
-      key: 'rut',
-      value: this.rut
-    });
-
-    await Storage.set({
-      key: 'carrera',
-      value: this.carrera
-    });
-
-    await Storage.set({
-      key: 'email',
-      value: this.email
-    });
-
-    await Storage.set({
-      key: 'contrasena',
-      value: this.contrasena
-    });
+  async obtenerDatos() {
+    const usuarios = await Storage.get({ key: 'usuarios' });
+    this.usuarios = usuarios.value ? JSON.parse(usuarios.value) : [];
   }
 }
