084e79a16b78d353617598c1d31f97955aff9e0f
RazielAuditore <107446288+RazielAuditore@users.noreply.github.com>
2023-11-11 19:53:55 -0300

    recuperar pass
    devolucion de usuario

--- registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
+++ registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+import { Usuario } from '../registro/registro.page';
+
 
 @Component({
   selector: 'app-recuperar-pass',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPassPage implements OnInit {
 
+  usuarios: Usuario[] = [];
+  nombre: string = "";
+  edad: number = 0;
+  rut: string = "";
+  carrera: string = "";
+  email: string = "";
+  contrasena: string = "";
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
-}
+  async ObtenerDatos() {
+   
+      const usuarios = await Storage.get({ key: 'usuarios' });
+      this.usuarios = usuarios.value ? JSON.parse(usuarios.value) : [];
+    }
+
+    async obtenerUsuarioPorEmail(email: string): Promise<Usuario | undefined> {
+      await this.
+    ObtenerDatos(); // Asegurarse de tener los datos actualizados
+  
+      const usuarioEncontrado = this.usuarios.find((usuario) => usuario.email === email);
+  
+      return usuarioEncontrado;
+    }
+  }
+
+  
+
+
+
