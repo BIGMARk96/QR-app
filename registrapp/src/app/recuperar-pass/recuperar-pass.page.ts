47a363cc34fd26606fdaa12b6951f488d293fe25
RazielAuditore <107446288+RazielAuditore@users.noreply.github.com>
2023-11-11 22:25:43 -0300

    recuperra pass
    avanze pero no guarda ni arroja

--- registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
+++ registrapp/src/app/recuperar-pass/recuperar-pass.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@capacitor/storage';
 import { Usuario } from '../registro/registro.page';
 
-
 @Component({
   selector: 'app-recuperar-pass',
   templateUrl: './recuperar-pass.page.html',
@@ -11,33 +10,44 @@ import { Usuario } from '../registro/registro.page';
 export class RecuperarPassPage implements OnInit {
 
   usuarios: Usuario[] = [];
-  nombre: string = "";
-  edad: number = 0;
-  rut: string = "";
-  carrera: string = "";
-  email: string = "";
-  contrasena: string = "";
+  emailUsuario: string = "";
+  usuarioEncontrado: Usuario | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.ObtenerDatos();
   }
 
   async ObtenerDatos() {
-   
-      const usuarios = await Storage.get({ key: 'usuarios' });
-      this.usuarios = usuarios.value ? JSON.parse(usuarios.value) : [];
-    }
-
-    async obtenerUsuarioPorEmail(email: string): Promise<Usuario | undefined> {
-      await this.
-    ObtenerDatos(); // Asegurarse de tener los datos actualizados
-  
-      const usuarioEncontrado = this.usuarios.find((usuario) => usuario.email === email);
-  
-      return usuarioEncontrado;
-    }
+    const usuarios = await Storage.get({ key: 'usuarios' });
+    this.usuarios = usuarios.value ? JSON.parse(usuarios.value) : [];
   }
+
+  async obtenerUsuarioPorEmail(email: string): Promise<Usuario | undefined> {
+    await this.ObtenerDatos();
+    const usuarioEncontrado = this.usuarios.find((usuario) => usuario.email === email);
+    return usuarioEncontrado;
+  }
+
+  buscarUsuario() {
+    this.obtenerUsuarioPorEmail(this.emailUsuario)
+      .then(usuario => {
+        this.usuarioEncontrado = usuario;
+        if (usuario) {
+          console.log('Usuario encontrado:', usuario);
+        } else {
+          console.log('Usuario no encontrado');
+        }
+      })
+      .catch(error => console.error('Error al buscar usuario:', error));
+  }
+
+  
+}
+
+    
+    
 
   
 
