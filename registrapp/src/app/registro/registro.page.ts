0e6e5fccdc12571e3ea402a7a79e9233038803a2
jupizarro <ju.pizarro@duocuc.cl>
2023-11-15 12:03:20 -0300

    Se agrega Ciudad/comuna

--- registrapp/src/app/registro/registro.page.ts
+++ registrapp/src/app/registro/registro.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@capacitor/storage';
+import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 export class Usuario {
   nombre: string = "";
@@ -9,6 +13,9 @@ export class Usuario {
   email: string = "";
   contrasena: string = "";
   token: boolean = false;
+  selectedCity: { id: number; name: string } = { id: 0, name: '' };
+  selectedCommune: string = '';
+
 }
 
 @Component({
@@ -25,9 +32,27 @@ export class RegistroPage implements OnInit {
   email: string = "";
   contrasena: string = "";
   token: boolean = false;
+  selectedCity: { id: number; name: string } = { id: 0, name: '' };
+  selectedCommune: string = '';
+  cities: { id: number; name: string }[] = [];
+  communes: { id: number; name: string }[] = [];
 
-  ngOnInit(): void {
-    // Puedes agregar código de inicialización aquí si es necesario
+  constructor(private navCtrl: NavController,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer,){}
+
+  ngOnInit(){
+    this.http.get<any>('https://dev.matiivilla.cl/duoc/location/region').subscribe({
+      next: (data) => {
+        this.cities = data.data.map((city: any) => ({
+          id: city.id,
+          name: city.nombre,
+        }));
+      },
+      error: (error) => {
+        console.error('Error al obtener la lista de ciudades:', error);
+      },
+    });
   }
 
   async guardarDatos() {
@@ -39,6 +64,8 @@ export class RegistroPage implements OnInit {
     usuario.email = this.email;
     usuario.contrasena = this.contrasena;
     usuario.token = this.token;
+    usuario.selectedCity = this.selectedCity;
+    usuario.selectedCommune = this.selectedCommune;
 
     this.usuarios.push(usuario);
 
@@ -48,6 +75,20 @@ export class RegistroPage implements OnInit {
     });
   }
   
+  loadCommunes(city: { id: number; name: string }) {
+    const cityId = city.id;
+    this.http.get<any>(`https://dev.matiivilla.cl/duoc/location/comuna/${cityId}`).subscribe({
+      next: (data) => {
+        this.communes = data.data.map((commune: any) => ({
+          id: commune.id,
+          name: commune.nombre,
+        }));
+      },
+      error: (error) => {
+        console.error('Error al obtener la lista de comunas:', error);
+      },
+    });
+  }
 
   async obtenerDatos() {
     const usuarios = await Storage.get({ key: 'usuarios' });
